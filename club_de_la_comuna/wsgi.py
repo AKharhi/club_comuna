98227c7c4ccd49962a887b7c25dcd6b1babbafca
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-10-10 01:38:37 -0300

    wsgi

--- club_de_la_comuna/wsgi.py
+++ club_de_la_comuna/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "club_de_la_comuna.club_de_la_comuna.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "club_de_la_comuna.settings")
 
 application = get_wsgi_application()
