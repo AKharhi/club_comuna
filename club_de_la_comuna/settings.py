c9ba22cc656c728414d1acab4d612e47d0905d36
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-10-12 16:59:37 -0300

    cambiobd

--- club_de_la_comuna/settings.py
+++ club_de_la_comuna/settings.py
@@ -27,9 +27,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key-for-devel
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
+
 
 
 # Application definition
