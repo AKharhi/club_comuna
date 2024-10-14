b7616d33e988ae2dd57c9290e2aebf36a78d2ef0
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-10-13 21:29:08 -0300

    img_default_model

--- core/models.py
+++ core/models.py
@@ -3,18 +3,18 @@ from django.core.validators import validate_email
 
 
 class Negocio(models.Model):
-    nombre = models.CharField(max_length=100)  # Agregué max_length
-    direccion = models.CharField(max_length=200)  # Agregué max_length
-    horario = models.CharField(max_length=100)  # Agregué max_length
-    telefono = models.CharField(max_length=15)  # Agregué max_length (puede variar dependiendo del formato de teléfono)
+    nombre = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=200)
+    horario = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=15)
     email = models.EmailField(max_length=100, validators=[validate_email])
-
     descripcion = models.TextField()
-    imagen = models.ImageField(upload_to='negocios/')  # Agregué un valor para upload_to para definir el directorio
+    
+    # Proporciona un valor predeterminado para el campo 'imagen'
+    imagen = models.ImageField(upload_to='negocios/', default='negocios/default.jpg')
+    
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.nombre)  # Convertimos el valor a una cadena
-
-
+        return str(self.nombre)
