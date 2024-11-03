46140962c862ee0a187cdedf29e4e214607714e9
Ana Karina Muñoz Espinoza <an.munoze@duocuc.cl>
2024-11-02 23:55:16 -0300

    login_gmailok

--- club_de_la_comuna/settings.py
+++ club_de_la_comuna/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = "club_de_la_comuna.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
