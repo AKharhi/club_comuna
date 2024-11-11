81fdf5a1865fd7bb33f3cd4fd63697a39ced71a5
NADIA ARELLANO <nadia.arellano.g@gmail.com>
2024-11-10 22:36:31 -0300

    Mejora frontend base y el club

--- club_de_la_comuna/urls.py
+++ club_de_la_comuna/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('chat/start/', views.start_conversation, name='start_conversation'),
     path('chat/send/', views.send_message, name='send_message'),
     path('como_unirse/', views.como_unirse, name='como_unirse'),
+    path('usuario_logueado/', views.usuario_logueado, name='usuario_logueado'),
     path('accounts/', include('allauth.urls')),  # Incluye las URLs de allauth
     path('negocios_con_ofertas_activas/', views.negocios_con_ofertas_activas, name='negocios_con_ofertas_activas')
 ]
